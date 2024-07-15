6ea166c9ac55fec9fd2b5f1f732e5b204bfc3adf
curlysame <118316442+curlysame@users.noreply.github.com>
2024-07-14 22:39:50 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+#calculadora
+num1 = float(input("digite o primeiro número: "))
+num2 = float(input("digite o segundo número: "))
+
+adi = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+
+
+print('Resultado da adição : ', adi)
+print('Resultado da subtração: ', sub)
+print('Resultado da divisão: ', div)
+print('Resultado da multiplicação: ', mul)
